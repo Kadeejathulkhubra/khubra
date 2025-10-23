2a269f7cdfe5909bb64a51a1854b4e59bec4d5d3
kadeejathul khubra <khubrakadheejathul@gmail.com>
2025-10-23 21:00:02 +0530

    first commit

---
+++ hashing.py
@@ -0,0 +1,17 @@
+
+#password hashing
+
+import hashlib
+
+password = "Cyber@123"
+
+# MD5 (not secure for real use)
+md5_hash = hashlib.md5(password.encode()).hexdigest()
+
+# SHA256 (secure)
+sha256_hash = hashlib.sha256(password.encode()).hexdigest()
+
+print("MD5:", md5_hash)
+print("SHA256:", sha256_hash)
+
+
